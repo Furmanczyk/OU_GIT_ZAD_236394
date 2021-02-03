9b0a9a4cbe8f1f359375fdceb45c44c430bb649a
Furmanczyk <furmanczyk.dominik7@gmail.com>
2021-02-03 23:27:44 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_GIT_ZAD_236394.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_GIT_ZAD_236394.dir/main.c.obj"
+  "OU_GIT_ZAD_236394.exe"
+  "OU_GIT_ZAD_236394.exe.manifest"
+  "OU_GIT_ZAD_236394.pdb"
+  "libOU_GIT_ZAD_236394.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_GIT_ZAD_236394.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
